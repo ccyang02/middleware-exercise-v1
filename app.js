75b373b44f5b81f1b2b13c4b8a541b7dd8d35b11
ccyang02 <chiachingyang02@gmail.com>
2020-10-27 23:38:40 +0800

    first init & feat: add timestamp logs

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+require('console-stamp')(console, {
+  format: ':date(yyyy-mm-dd HH:MM:ss).yellow :label'
+});
+
+app.use(function (req, res, next) {
+  console.log(`${req.method} from ${req.originalUrl}`)
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.send('列出全部 Todo')
+})
+
+app.get('/new', (req, res) => {
+  res.send('新增 Todo 頁面')
+})
+
+app.get('/:id', (req, res) => {
+  res.send('顯示一筆 Todo')
+})
+
+app.post('/', (req, res) => {
+  res.send('新增一筆  Todo')
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`)
+})
